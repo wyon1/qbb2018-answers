e23fddaaeba8b40c41652f6d0bbf847323e9a986
wyon1 <wyon@jhu.edu>
2018-08-28 13:35:56 -0400

    Exercise 2

---
+++ day2-lunch/day2-exercise-2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) > 1:
+	f = open(sys.argv[1])
+else:
+	f = sys.stdin
+
+#for i in range(10):
+ #   print(f.readline().rstrip)
+count = 0
+for i, line in enumerate(f):
+	#one way to skip header
+	if i == 0:
+		continue
+	fields = line.rstrip("\r\n").split("\t")
+	if fields[1].startswith("@"):
+		continue
+	if len(fields) > 12:
+		continue
+		if fields[12] != "AS:i:0":
+			continue
+	else:
+		count += 1
+
+print(count)
