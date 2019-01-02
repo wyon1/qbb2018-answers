6e9876bf8a45800d8d6d8f24612fa6083af0492d
Wesley Yon <wesley@Wesleys-MacBook-Pro.local>
2019-01-02 16:03:29 -0500

    week 1

--- week1/aa_na_aligner.py
+++ week1/aa_na_aligner.py
@@ -5,125 +5,95 @@ import fasta
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
+import math
+
+aa_reader = fasta.FASTAReader(open(sys.argv[1]))
+dna_reader = fasta.FASTAReader(open(sys.argv[2]))
+
+dic = {}
+Z_test = []
+diff = []
+
+sig = []
+sig_pos = []
+
+non_sig = []
+non_sig_pos = []
+
+rel = 0
+
+
+for (dna_ident, dna), (aa_ident, aa) in zip(dna_reader, aa_reader):
+   j = 0
+   gaps = []
+   AA = []
+   for i in range(len(aa)):
+       AA.append(aa[i])
+
+       if aa[i] == "-":
+           gaps.append("-")
+           gaps.append("-")
+           gaps.append("-")
+       else:
+           gaps += dna[j:j+3]
+           j = j + 3
+
+   if rel == 1:
+       dic[aa] = "".join(gaps)
+   elif rel == 0:
+       aa_seq = AA
+       dna_seq = gaps
+
+       count1=[0]*len(aa_seq)
+       count2=[0]*len(aa_seq)
+       rel = 1
+
+for aa in dic:
+   for i in range(len(aa)):
+       if aa[i] =="-":
+           continue
+       elif aa[i] != aa_seq[i]:
+           count1[i] = count1[i]+1
+       elif dic[aa][3*i] != dna_seq[3*i] or dic[aa][3*i+1] != dna_seq[3*i+1] or dic[aa][3*i+2] != dna_seq[3*i+2]:
+           count2[i] = count2[i]+1
 
 
 
-dna_reader = fasta.FASTAreader(open(sys.argv[1]))
-aa_reader = fasta.FASTAreader(open(sys.argv[2]))
-new_seq = []
+for i in range(len(aa_seq)):
+   diff.append(count1[i]-count2[i])
+   x = len(aa_seq)
+
+   Z_test.append((diff[i]-0)/((np.std(diff))/(math.sqrt(x))))
 
 
-for (dna_ident, dna) , (aa_ident, aa) in zip(dna_reader, aa_reader):
-	j = 0
-	gaps = []
-	for i in range(len(aa)):
-		a = aa[i]
-		if a == "-":
-			gaps.append("---")
-		else:
-			gaps.append(dna[j:j+3])
-			j += 3
-	new_seq.append(gaps)
 
-#every codon permutation
-all_codons = ['---','ATT', 'ATC', 'ATA','CTT', 'CTC', 'CTA', 'CTG', 'TTA', 'TTG','GTT', 'GTC', 'GTA', 'GTG','TTT', 'TTC','ATG','TGT', 'TGC','GCT', 'GCC', 'GCA', 'GCG','GGT', 'GGC', 'GGA', 'GGG','CCT', 'CCC', 'CCA', 'CCG','ACT', 'ACC', 'ACA', 'ACG','TCT', 'TCC', 'TCA', 'TCG', 'AGT', 'AGC','TAT', 'TAC','TGG','CAA', 'CAG','AAT', 'AAC','CAT', 'CAC','GAA', 'GAG','GAT', 'GAC','AAA', 'AAG','CGT', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG','TAA', 'TAG', 'TGA']
+for i in range(len(aa_seq)):
+   dN = count1[i]
 
-# this dictionary shows which codons encode the same AA 
-SynonymousCodons = {
-    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
-    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
-    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
-    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
-    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
-    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
-    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
-    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
-    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
-    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
-    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
-    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
-    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
-    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
-    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
-    '---':'-', 'GA':'','GG':'','GC':'', 'G':'','A':'','C':'','T':'','GT':'','AG':'','AC':'', 'AT':'', 'AA':'', 'CT':'', 'CG':'', 'CA':'', 'CC':'', 'TT':'', 'TC':'', 'TG':'', 'TA':''}
+   if count2[i] == 0:
+       dS = 1
+   else:
+       dS = count2[i]
+
+   if Z_test[i] > 2.58:
+       sig.append(np.log2(dN/dS))
+       sig_pos.append(i+1)
+   else:
+       non_sig.append(np.log2(dN/dS))
+       non_sig_pos.append(i+1)
 
 
-num_of_dS = []
 
-num_of_dN = []
-
-
-for j in range(0,4850):
-#	print(SynonymousCodons[new_seq[0][j]])
-	for i in range(1, len(new_seq)):
-		dS = 0
-		dN = 0
-		if new_seq[0][j] not in str(all_codons) or new_seq[i][j] not in str(all_codons):
-			continue
-		if new_seq[0][j] == new_seq[i][j]:
-			continue
-		elif SynonymousCodons[new_seq[0][j]] != SynonymousCodons[new_seq[i][j]]:
-			dN += 1
-		else:
-			dS += 1
-		num_of_dS.append(dS)
-		num_of_dN.append(dN)
-	else:
-		continue
-Dc = np.subtract(num_of_dN, num_of_dS)
-mean_Dc = np.mean(Dc)
-standev = np.std(Dc)
-SEM = stats.sem(Dc)
-
-z_values = []
-significant = []
-
-for i in np.nditer(Dc):
-	x = abs(mean_Dc - i) / SEM
-	z_values.append(x)
-print (z_values)
-
-for i in np.nditer(Dc):
-	if abs(np.subtract(mean_Dc, Dc[i])) > standev:
-		significant.append('1')
-	else:
-		significant.append('0')
-
-
-"""
 fig, ax = plt.subplots()
 
-x = num_of_dS
-y = num_of_dN
+ax.scatter(sig_pos, sig, s=2, color="orange")
+ax.scatter(non_sig_pos, non_sig, s=2, color="blue")
+
+ax.set_xlabel("Condon position")
+ax.set_ylabel("Z-test result")
+ax.set_title("dN/dS per codon position")
 
 
-ax.scatter(x,y)
+fig.savefig("dN_to_dS.png")
 
-plt.xlabel('num_of_dS')
-plt.ylabel('num_of_dN')
-#plt.ylim(1, 10**5)
-#plt.xlim(1, 10**5)
-
-
-fig.savefig("dS_to_dN.jpg")
 plt.close(fig)
-#ax.set_title("Descriptive Title Here")
-
-
-
-
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
--- week1/fasta.py
+++ week1/fasta.py
@@ -1,47 +1,45 @@
 #!/usr/bin/env python3
 
 """
-Parse all FASTA record from stdin and print id and sequence
+Parse a single FASTA record from stdin and print id and sequence
 """
 
 import sys
 
-class FASTAreader(object):
-	
-	def __init__(self, file):
-		self.last_ident = None
-		self.file = file
-		self.eof = False
+class FASTAReader( object ):
 
-	def __iter__(self):
-		return self
+    def __init__(self, file):
+        self.last_ident = None
+        self.file = file
+        self.eof = False
 
+    def __iter__(self):
+        return self
 
-	def __next__(self):
-		if self.eof:
-			raise StopIteration
-		if self.last_ident is not None:
-			#Not first line
-			ident = self.last_ident
+    def __next__(self):
+        if self.eof:
+            #return None, None
+            raise StopIteration
+        if self.last_ident is not None:
+            ident = self.last_ident   
+        else:
+            line = self.file.readline()
+            if line == "":
+                return None, None
+            assert line.startswith(">"), "Not a FASTA file"
+            ident = line[1:].rstrip("\r\n")
+            
+        sequences = []
+        while True:
+            line = self.file.readline()
+            if line == "":
+                self.eof = True
+                break
+            elif not line.startswith(">"):
+                sequences.append( line.strip() )
+            else:
+                self.last_ident = line[1:].rstrip("\r\n")
+                break
 
-		else:
-			line = self.file.readline()
-			if line == "":
-				return None, None
-			assert line.startswith(">"), "Not a FASTA file"
-			ident = line[1:].rstrip("\r\n")
-
-		sequences = []
-		while True:
-			line = self.file.readline()
-			if line == "":
-				self.eof = True
-				break
-			elif not line.startswith(">"):
-				sequences.append(line.strip())
-			else:
-				self.last_ident = line[1:].rstrip("\r\n")
-				break
-
-		sequence = "".join(sequences)
-		return ident, sequence
+        sequence = "".join( sequences )
+        return ident, sequence
